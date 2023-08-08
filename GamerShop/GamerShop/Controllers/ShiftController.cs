ebf6a0c2f1626142cda3ea13dcbba59f6ac8babd
Aliaksei Pustavy <wix1817@gmail.com>
2023-08-08 21:08:05 +0200

    Added functionality: adding, deleting and displaying elements

---
+++ GamerShop/GamerShop/Controllers/ShiftController.cs
@@ -0,0 +1,46 @@
+﻿using GamerShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers;
+
+public class ShiftController : Controller
+{
+    public static List<ShiftViewModel> Shifts = new List<ShiftViewModel>();
+    
+    public IActionResult Index()
+    {
+        return View(Shifts);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Add(ShiftViewModel shiftViewModel)
+    {
+        if (Shifts.Count != 0)
+        {
+            shiftViewModel.Id = Shifts.Last().Id + 1;
+        }
+        else
+        {
+            shiftViewModel.Id = 0;
+        }
+        Shifts.Add(shiftViewModel);
+        return View();
+    }
+    
+    public IActionResult Delete(int id)
+    {
+        var shiftToDelete = Shifts.FirstOrDefault(shift => shift.Id == id);
+        if (shiftToDelete != null)
+        {
+            Shifts.Remove(shiftToDelete);
+        }
+        return RedirectToAction("Index", "Shift");
+    }
+    
+}
