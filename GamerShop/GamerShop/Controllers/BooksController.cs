8fd04cf23457060ea07c655ba44f423d97b91a39
Elizaveta Glushchenko <elizaveta.glushchenko@softeq.com>
2023-08-09 20:32:58 +0300

    Commit Add books

---
+++ GamerShop/GamerShop/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using GamerShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers
+{
+    public class BooksController : Controller
+    {
+        public static List<BookViewModel> _books = new List<BookViewModel>();
+
+        [HttpGet]
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Book(BookViewModel bookViewModel)
+        {
+            if (bookViewModel.Name != String.Empty && bookViewModel.Author != String.Empty)
+            {
+                    _books.Add(bookViewModel);
+            }
+
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            return View(_books);
+        }
+    }
+}
---
+++ GamerShop/GamerShop/Models/BookViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GamerShop.Models
+{
+    public class BookViewModel
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int YearOfIssue { get; set; }
+    }
+}
