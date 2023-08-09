d31b1b6ff0025d741ae89ec095de95aeb3b79d09
kanura56@gmail.com <kanura56@gmail.com>
2023-08-09 14:50:13 +0300

    Added Recipes viewmodel, controller and views

---
+++ GamerShop/GamerShop/Controllers/RecipeController.cs
@@ -0,0 +1,38 @@
+﻿using GamerShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public static List<RecipeViewModel> Recipes = new();
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(RecipeViewModel recipeViewModel)
+        {
+	        if (!ModelState.IsValid)
+	        {
+		        return View();
+	        }
+			Recipes.Add(recipeViewModel);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+	        return View(Recipes);
+        }
+
+        public IActionResult Remove(int id)
+        { 
+	        Recipes.RemoveAt(id);
+	        return RedirectToAction("Show");
+        }
+	}
+}
