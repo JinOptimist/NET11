5d94eeeb9764a9d8467ed03e20aae66c43a1d8ed
Алексей Клиндюк <alklin01@gmail.com>
2023-08-09 15:48:30 +0300

    Add HW Lesson 18

---
+++ GamerShop/GamerShop/Controllers/RockHallController/RockHallController.cs
@@ -0,0 +1,45 @@
+﻿using GamerShop.Models;
+using GamerShop.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers.RockHallController
+{
+    public class RockHallController : Controller
+    {
+        public static List<RockMembers> _rockPerson = new List<RockMembers>()
+        {
+            new RockMembers("The Beatles", "Psychedelic Rock", 1994, 1956),
+            new RockMembers("Queen", "Pop Rock", 1999, 1958),
+            new RockMembers("John Bon Jovi", "Rock-N-Roll", 2001, 1973),
+            new RockMembers("The Who", "Psychedelic Rock", 1988, 1963),
+            new RockMembers("System Of Down", "Rock-N-Roll", 2003, 1993),
+            new RockMembers("The Offspring", "Punk Rock", 2011, 1993),
+            new RockMembers("System of a Down", "Hard Rock", 2001, 1973)
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_rockPerson);
+        }
+
+        public IActionResult Delete(int index)
+        {
+            _rockPerson.RemoveAt(index);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult NewRockMember()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewRockMember(RockFameViewModel rockFameViewModel)
+        {
+            _rockPerson.Add(new RockMembers(rockFameViewModel.FullName, rockFameViewModel.Genre, rockFameViewModel.EntryYear, rockFameViewModel.YearOfBirth));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GamerShop/GamerShop/Models/RockFameViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamerShop.Models
+{
+    public class RockFameViewModel
+    {
+
+        [Required(ErrorMessage = "Пустая пустота, не надо так...")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Пустая пустота, не надо так...")]
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Пустая пустота, не надо так...")]
+        [Display(Name = "Entry Year in Rock Hall (1950-2023)")]
+        [Range(1950, 2023, ErrorMessage = "Ты втираешь мне какую-то дичь")]
+        public int EntryYear { get; set; }
+
+        [Required(ErrorMessage = "Пустая пустота, не надо так...")]
+        [Display(Name = "Year Of Birth")]
+        public int YearOfBirth { get; set; }
+
+    }
+}
