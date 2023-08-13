da8a9dfa3fdbcb312366b2f87456c18736843795
Алексей Клиндюк <alklin01@gmail.com>
2023-08-13 20:38:52 +0300

    Fix namespace InfoMemberViewModel

--- GamerShop/GamerShop/Models/RockHall/InfoMemberViewModel.cs
+++ GamerShop/GamerShop/Models/RockHall/InfoMemberViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
-namespace DALInterfaces.Models
+namespace GamerShop.Models.RockHall
 {
     public class InfoMemberViewModel
     {
