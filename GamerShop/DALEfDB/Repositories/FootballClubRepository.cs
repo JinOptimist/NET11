a277edf9a66b0bc284d63e7216e296a84220acbe
Ardashirov Vladimir <vladimir-ardashirov@mail.ru>
2023-08-20 15:19:38 +0300

    Revert "Revert "Create new table for FootballClubs""
    This reverts commit 7c63a943e510695e74b5f7ae6e9ab6137ba450ab.

---
+++ GamerShop/DALEfDB/Repositories/FootballClubRepository.cs
@@ -0,0 +1,12 @@
+﻿using DALInterfaces.Models;
+using DALInterfaces.Repositories;
+
+namespace DALEfDB.Repositories
+{
+    public class FootballClubRepository : BaseRepository<FootballClub>, IFootballClubRepository
+    {
+        public FootballClubRepository(WebContext context) : base(context) { }
+
+
+    }
+}
