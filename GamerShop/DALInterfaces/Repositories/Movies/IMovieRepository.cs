b3a63f96b75f8212f8847153c0dfd4b5ba2c694c
Vlad <snytko.vlad@mail.ru>
2023-08-26 19:54:49 +0200

    Added repository interfaces.

---
+++ GamerShop/DALInterfaces/Repositories/Movies/IMovieRepository.cs
@@ -0,0 +1,7 @@
+﻿using DALInterfaces.Models.Movies;
+
+namespace DALInterfaces.Repositories.Movies;
+
+public interface IMovieRepository : IBaseRepository<Movie>
+{
+}
---
+++ GamerShop/DALInterfaces/Repositories/Movies/IRatingRepository.cs
@@ -0,0 +1,7 @@
+﻿using DALInterfaces.Models.Movies;
+
+namespace DALInterfaces.Repositories.Movies;
+
+public interface IRatingRepository : IBaseRepository<Rating>
+{
+}
