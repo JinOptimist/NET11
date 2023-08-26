2e9de6dfb0abd2ce2fb15fedaf656279e9caf0ee
Vlad <snytko.vlad@mail.ru>
2023-08-26 20:08:03 +0200

    Updated view models.

---
+++ GamerShop/GamerShop/Models/Movies/ShortCollectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GamerShop.Models.Movies;
+
+public class ShortCollectionViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public double Rating { get; set; }
+    public DateTime DateCreated { get; set; }
+}
