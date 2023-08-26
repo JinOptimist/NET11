9f747117224d2797786fe0872162bd7540c42d59
Vlad <snytko.vlad@mail.ru>
2023-08-26 20:06:21 +0200

    Added controllers.

---
+++ GamerShop/GamerShop/Controllers/Movies/CollectionController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayerInterfaces.MovieServices;
+using GamerShop.Models.Movies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers.Movies;
+
+public class CollectionController : Controller
+{
+    private readonly ICollectionService _collectionService;
+
+    public CollectionController(ICollectionService collectionService)
+    {
+        _collectionService = collectionService;
+    }
+
+    [HttpGet]
+    public IActionResult Show(int id)
+    {
+        var collectionBlm = _collectionService.GetCollectionById(id);
+        var collectionViewModel = new CollectionViewModel
+        {
+            Id = collectionBlm.Id,
+            Title = collectionBlm.Title,
+            Description = collectionBlm.Description,
+            DateCreated = collectionBlm.DateCreated,
+            AuthorName = collectionBlm.AuthorName,
+            Movies = collectionBlm.Movies,
+            Rating = collectionBlm.Rating
+        };
+
+        return View(collectionViewModel);
+    }
+}
---
+++ GamerShop/GamerShop/Controllers/Movies/SiteController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayerInterfaces.MovieServices;
+using GamerShop.Models.Movies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamerShop.Controllers.Movies;
+
+public class SiteController : Controller
+{
+    private readonly ICollectionService _collectionService;
+
+    public SiteController(ICollectionService collectionService)
+    {
+        _collectionService = collectionService;
+    }
+
+    [HttpGet]
+    public IActionResult Show()
+    {
+        var shortCollectionViewModels = _collectionService
+            .GetShortCollectionSortedByDate()
+            .Select(shortCollectionBlm => new ShortCollectionViewModel
+            {
+                Id = shortCollectionBlm.Id,
+                Title = shortCollectionBlm.Title,
+                Description = shortCollectionBlm.Description,
+                DateCreated = shortCollectionBlm.DateCreated,
+                Rating = shortCollectionBlm.Rating
+            })
+            .ToList();
+
+        return View(shortCollectionViewModels);
+    }
+}
