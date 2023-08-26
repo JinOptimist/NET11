681ea3af7dccb9ecf76da2445e2669fefc033f22
Vlad <snytko.vlad@mail.ru>
2023-08-26 19:59:20 +0200

    Added implementation of repository interfaces.

---
+++ GamerShop/DALEfDB/Repositories/Movies/MovieRepository.cs
@@ -0,0 +1,10 @@
+﻿using DALInterfaces.Models.Movies;
+using DALInterfaces.Repositories.Movies;
+
+namespace DALEfDB.Repositories.Movies
+{
+    public class MovieRepository : BaseRepository<Movie>, IMovieRepository
+    {
+        public MovieRepository(WebContext context) : base(context) { }
+    }
+}
