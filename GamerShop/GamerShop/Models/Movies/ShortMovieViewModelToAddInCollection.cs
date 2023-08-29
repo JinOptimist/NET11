05cd52f59c85567dcec68357c948c9024a4070cc
Vlad <snytko.vlad@mail.ru>
2023-08-29 22:50:50 +0200

    Replaced with SelectListItem.

--- GamerShop/GamerShop/Models/Movies/ShortMovieViewModelToAddInCollection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GamerShop.Models.Movies;
-
-public class ShortMovieViewModelToAddInCollection
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public bool IsSelected { get; set; }
-}
