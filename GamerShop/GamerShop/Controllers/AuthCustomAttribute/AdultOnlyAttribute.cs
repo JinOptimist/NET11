a1d315dca037c705ba5e9fef38cb351eb97c0ce0
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-08-30 20:11:55 +0300

    Add AuthCustomAttrubute

---
+++ GamerShop/GamerShop/Controllers/AuthCustomAttribute/AdultOnlyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using GamerShop.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GamerShop.Controllers.AuthCustomAttribute
+{
+    public class AdultOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // BEFORE ACTION RUN
+            var authService = context.HttpContext.RequestServices.GetService<IAuthService>();
+
+            if (authService.GetCurrentUser().AgeInDays < 17)
+            {
+                context.HttpContext.Response.Redirect("/Auth/PageNotForChild");
+                // DO NOT run next attribute
+                return;
+            }
+
+            // Run Next Atttibute
+            base.OnActionExecuting(context);
+        }
+    }
+}
