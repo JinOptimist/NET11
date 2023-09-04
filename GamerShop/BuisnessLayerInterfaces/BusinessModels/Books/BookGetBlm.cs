744f0cce03f5c3de209e8c2811a79dd80b9494e4
Elizaveta Glushchenko <elizaveta.glushchenko@softeq.com>
2023-09-04 18:38:54 +0300

    Remove base blm

--- GamerShop/BuisnessLayerInterfaces/BusinessModels/Books/BookGetBlm.cs
+++ GamerShop/BuisnessLayerInterfaces/BusinessModels/Books/BookGetBlm.cs
@@ -1,7 +1,11 @@
 ﻿namespace BusinessLayerInterfaces.BusinessModels.Books
 {
-    public class BookGetBlm : BookBaseBlm
-    {
+    public class BookGetBlm
+    { 
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int YearOfIssue { get; set; }
         public string CreatorName { get; set; }
     }
 }
