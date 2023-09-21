2b997ebc6afce6365aaeb5a5c5dcae44278a2093
Vlad <snytko.vlad@mail.ru>
2023-09-21 21:25:57 +0200

    Added toggle checkbox for IsPublic.

---
+++ GamerShop/GamerShop/wwwroot/js/toogle.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('toggle');
+    const toggleText = document.querySelector('.toggle-text');
+
+    toggle.addEventListener('change', function () {
+        if (toggle.checked) {
+            toggleText.textContent = toggleText.getAttribute('data-on');
+        } else {
+            toggleText.textContent = toggleText.getAttribute('data-off');
+        }
+    });
+});
