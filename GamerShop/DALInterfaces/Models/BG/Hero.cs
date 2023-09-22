f83246679741c98b4acbf5bd9280e90377d9fd39
Slava Drozd <qscokmvbn@mail.ru>
2023-09-22 11:17:48 +0300

    Create BD

---
+++ GamerShop/DALInterfaces/Models/BG/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALInterfaces.Models.BG
+{
+    public class Hero : BaseModel
+    {
+
+        public string Name { get; set; }
+        public int Bone { get; set; }
+
+        public virtual Race Race { get; set; }
+        public virtual Subrace Subrace { get; set; }
+        public virtual Class  Class{ get; set; }
+        public virtual Оrigin Оrigin { get; set; }
+        public virtual User UserCreator { get; set; }
+
+    }
+}
