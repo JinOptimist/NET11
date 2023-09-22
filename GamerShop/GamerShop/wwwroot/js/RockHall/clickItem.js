ab02628b6ae10702b77ae7949a45e39d47ede4ab
Алексей Клиндюк <alklin01@gmail.com>
2023-09-22 21:56:38 +0300

    Js start

---
+++ GamerShop/GamerShop/wwwroot/js/RockHall/clickItem.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(".content").click(function () {
+        $(this).animate({ left: '100px' }, "slow");
+        $(this).animate({ fontSize: '3em' }, "slow");
+    });
+
+});
+
