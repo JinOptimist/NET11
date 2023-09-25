81b0680c228411db76155ff9afb3f59216d4f0b1
Elizaveta Glushchenko <elizaveta.glushchenko@softeq.com>
2023-09-25 17:32:03 +0300

    Move book into folder

---
+++ GamerShop/DALInterfaces/Models/Books/Book.cs
@@ -0,0 +1,13 @@
+﻿using DALInterfaces.Models.Movies;
+using DALInterfaces.Models.RockHall;
+
+namespace DALInterfaces.Models.Books
+{
+    public class Book : BaseModel
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int YearOfIssue { get; set; }
+        public virtual ICollection<Movie> FilmAdaptations { get; set; }
+    }
+}
