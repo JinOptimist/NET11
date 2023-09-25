4792a577683e0797f54b0a0429e8e52ee0a0603d
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-09-25 10:12:08 +0300

    Fix SnapShot

--- GamerShop/DALEfDB/Migrations/20230925070917_FakeV2.Designer.cs
+++ GamerShop/DALEfDB/Migrations/20230925070917_FakeV2.Designer.cs
@@ -4,6 +4,7 @@ using DALEfDB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DALEfDB.Migrations
 {
     [DbContext(typeof(WebContext))]
-    partial class WebContextModelSnapshot : ModelSnapshot
+    [Migration("20230925070917_FakeV2")]
+    partial class FakeV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
