4c7eec34263a209594a9fcebf09dcd15b8fe6df8
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-10-02 21:22:44 +0300

    Generate pdf in separated thread

--- GamerShop/GamerShop/Controllers/UserController.cs
+++ GamerShop/GamerShop/Controllers/UserController.cs
@@ -10,36 +10,62 @@ namespace GamerShop.Controllers
 {
     [Authorize]
     public class UserController : Controller
-	{
-		private IUserService _userService;
+    {
+        private IUserService _userService;
         private IWebHostEnvironment _webHostEnvironment;
         private Services.IAuthService _authService;
+        private IPdfGeneratorService _pdfGeneratorService;
 
         private IPaginatorService _paginatorService;
 
-        public UserController(IUserService userService, 
-            IPaginatorService paginatorService, 
-            IWebHostEnvironment webHostEnvironment, 
-            Services.IAuthService authService)
+        private static Dictionary<int, IPdfGeneratorService> _currentPdfJobs = new Dictionary<int, IPdfGeneratorService>();
+
+        public UserController(IUserService userService,
+            IPaginatorService paginatorService,
+            IWebHostEnvironment webHostEnvironment,
+            Services.IAuthService authService,
+            IPdfGeneratorService pdfGeneratorService)
         {
             _userService = userService;
             _paginatorService = paginatorService;
             _webHostEnvironment = webHostEnvironment;
             _authService = authService;
+            _pdfGeneratorService = pdfGeneratorService;
         }
 
 
         public IActionResult Index(int page = 1, int perPage = 10)
-		{
-			var viewModel = _paginatorService
-				.GetPaginatorViewModel(_userService, MapBlmToViewModel, page, perPage);
-			return View(viewModel);
-		}
+        {
+            var viewModel = _paginatorService
+                .GetPaginatorViewModel(_userService, MapBlmToViewModel, page, perPage);
+            return View(viewModel);
+        }
 
         [HttpGet]
         public IActionResult Profile()
         {
-            return View();
+            var viewModel = new ProfileViewModel();
+            var currentUserId = _authService.GetCurrentUser().Id;
+            if (!_currentPdfJobs.ContainsKey(currentUserId))
+            {
+                return View(viewModel);
+            }
+
+            var pdfGenerator = _currentPdfJobs[currentUserId];
+            decimal perfomedUserCountDec = pdfGenerator.PerfomedUserCount;
+            decimal allUserCountDec = pdfGenerator.AllUserCount;
+            viewModel.CurrentJobProgress = Math.Round(perfomedUserCountDec / allUserCountDec, 2) * 100;
+            viewModel.IsFileReady = pdfGenerator.IsReady;
+
+            return View(viewModel);
+        }
+
+        public IActionResult DownloadReport()
+        {
+            var userId = _authService.GetCurrentUser().Id;
+            var path = _currentPdfJobs[userId].ResultPath;
+            var fileStream = new FileStream(path, FileMode.Open);
+            return File(fileStream, "application/pdf");
         }
 
         [HttpPost]
@@ -48,7 +74,7 @@ namespace GamerShop.Controllers
             var userId = _authService.GetCurrentUser().Id;
             var fileName = $"{userId}.jpg";
             var path = Path.Combine(
-                _webHostEnvironment.WebRootPath, 
+                _webHostEnvironment.WebRootPath,
                 "img",
                 "avatars",
                 fileName);
@@ -62,32 +88,32 @@ namespace GamerShop.Controllers
         }
 
         private UserViewModel MapBlmToViewModel(UserBlm userBlm)
-		{
-			return new UserViewModel
-			{
-				Id = userBlm.Id,
-				Name = userBlm.Name,
-				FavMovieName = userBlm.FavoriteMovieName,
-				AgeInDays = userBlm.AgeInDays,
-			};
+        {
+            return new UserViewModel
+            {
+                Id = userBlm.Id,
+                Name = userBlm.Name,
+                FavMovieName = userBlm.FavoriteMovieName,
+                AgeInDays = userBlm.AgeInDays,
+            };
         }
 
-        [AdultOnly]
-        public IActionResult LookAtSome18PlusPage()
-		{
-			return View();
-		}
-
-        [AdultOnly]
-        public IActionResult DoAtSome18PlusPage()
+        public IActionResult GeneratePdfWithUserNames()
         {
-            return View();
-        }
+            var userId = _authService.GetCurrentUser().Id;
+            var fileName = $"{userId}.pdf";
+            var path = Path.Combine(
+                _webHostEnvironment.WebRootPath,
+                "reports",
+                fileName);
 
-        [AdultOnly]
-        public IActionResult FindAtSome18PlusPage()
-        {
-            return View();
+            var curentUserId = _authService.GetCurrentUser().Id;
+            var userNames = _userService.GetAllUserNames();
+            var task = new Task(() => _pdfGeneratorService.GeneratePdfWithUsers(path, userNames));
+            _currentPdfJobs.Add(curentUserId, _pdfGeneratorService);
+            task.Start();
+
+            return Json("We start the work");
         }
     }
 }
---
+++ GamerShop/TaskExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskExample;
+
+Console.WriteLine("Hello, World!");
+
+var bot = new DoStuff();
+//bot.CountToUnfinity("Wally");
+//bot.CountToUnfinity("Eva");
+
+var taskForWally = new Task(() => bot.CountToUnfinity("Wally"));
+taskForWally.Start();
+
+var taskForEva = new Task(() => bot.CountToUnfinity("Eva"));
+taskForEva.Start();
+
+
+Console.ReadLine();
