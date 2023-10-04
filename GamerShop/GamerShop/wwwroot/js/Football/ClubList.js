cf6cedc5dd78272e6a5c42c6d52887ea17db985e
Ardashirov Vladimir <vladimir-ardashirov@mail.ru>
2023-10-04 10:32:13 +0300

    add filter on html

--- GamerShop/GamerShop/wwwroot/js/Football/ClubList.js
+++ GamerShop/GamerShop/wwwroot/js/Football/ClubList.js
@@ -5,5 +5,24 @@
     $('.shows div:eq(0)').animate({ opacity: 1 }, 200, function () {
         $(this).next().animate({ opacity: 1 }, 200, arguments.callee);//с использованием ссылки на функцию-родитель arguments.callee.
     });
+    $('#accept').click(function () {
+        $('#exampleModal').modal('toggle');
+        let count = CountChecked(document.querySelectorAll('.form-check-input'));
+        $('#filter').text((count == 0) ? 'Установить фильтр' : 'Фильтров ' + count);
+
+    });
+
+    function CountChecked(elemtsArray) {
+
+        let count = 0;
+
+        for (var i = 0; i < elemtsArray.length; i++) {
+
+            if (elemtsArray[i].checked) {
+                count++;
+            }
+        }
+        return count
+    }
 });
 
