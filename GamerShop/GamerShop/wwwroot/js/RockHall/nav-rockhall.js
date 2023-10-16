77e962700e9b32e456f439c8a754e2160308d261
Алексей Клиндюк <alklin01@gmail.com>
2023-10-16 06:30:55 +0300

    Add JS

---
+++ GamerShop/GamerShop/wwwroot/js/RockHall/nav-rockhall.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('header').find('li').each(function () {
+        let currentSection = location.pathname == $('a', this).attr('href');
+        console.log(currentSection);
+        if(currentSection){
+            $('span', this).addClass('active-color');
+        }
+    })
+    console.log(location.pathname);
+});
